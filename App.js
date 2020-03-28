0489d2a5336fc7f6dc5b2fba64bc32618e987da6
Lauri Mäkinen <lauri@mits.fi>
2020-03-28 11:33:48 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text style={{ padding: 40, fontSize: 30, textAlign: 'center' }}>
+        Recycle Maps
+      </Text>
+    </View>
+  );
+}
