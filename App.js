e11a401b5b295cb90bae7a2d4c1bc8e1977c813d
Lauri Mäkinen <lauri@mits.fi>
2020-03-28 12:20:14 +0200

    Default to current geo position

--- App.js
+++ App.js
@@ -4,10 +4,24 @@ import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class App extends Component {
   state = {
-    search: '',
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((GeolocationPosition) => {
+      this.setState({
+        latitude: GeolocationPosition.coords.latitude,
+        longitude: GeolocationPosition.coords.longitude,
+      })
+    });
   }
 
   render() {
+    const { latitude, longitude, latitudeDelta, longitudeDelta } = this.state;
+
     return (
       <View>
         <Text style={styles.header}>
@@ -17,13 +31,9 @@ export default class App extends Component {
         <View style={styles.container}>
           <MapView
             provider={PROVIDER_GOOGLE}
-            region={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            }}
+            region={{ latitude, longitude, latitudeDelta, longitudeDelta }}
             style={styles.map}
+            showsUserLocation
           />
         </View>
       </View>
