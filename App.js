1ea0b207f9678b618368c0f46395cc6a034672c4
Lauri Mäkinen <lauri@mits.fi>
2020-03-28 12:54:56 +0200

    Move MapView in to its own file

--- App.js
+++ App.js
@@ -1,76 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-const markers = [
-  { title: "Paikka 1", coordinate: { latitude: 60.322344, longitude: 24.852691 } },
-  { title: "Paikka 2", coordinate: { latitude: 60.328479, longitude: 24.860194 } },
-];
+import MapView from './src/views/MapView';
 
 export default class App extends Component {
-  state = {
-    latitude: 0,
-    longitude: 0,
-    latitudeDelta: 0.02,
-    longitudeDelta: 0.02,
-  }
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition((GeolocationPosition) => {
-      this.setState({
-        latitude: GeolocationPosition.coords.latitude,
-        longitude: GeolocationPosition.coords.longitude,
-      })
-    });
-  }
-
   render() {
-    const { latitude, longitude, latitudeDelta, longitudeDelta } = this.state;
-
     return (
-      <View>
-        <Text style={styles.header}>
-          Recycle Maps
-        </Text>
-
-        <View style={styles.container}>
-          <MapView
-            provider={PROVIDER_GOOGLE}
-            mapType="hybrid"
-            region={{ latitude, longitude, latitudeDelta, longitudeDelta }}
-            style={styles.map}
-            showsUserLocation
-          >
-            {
-              markers.map(m => (
-                <MapView.Marker
-                  title={m.title}
-                  coordinate={m.coordinate}
-                  key={m.title}
-                />
-              ))
-            }
-          </MapView>
-        </View>
-      </View>
+      <MapView />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  header: {
-    paddingTop: 40,
-    paddingBottom: 20,
-    fontSize: 30,
-    textAlign: 'center',
-  },
-  container: {
-    height: 630,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
