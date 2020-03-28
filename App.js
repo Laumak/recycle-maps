c8c7b81149771c5cf8f1689fdcf8c0c09966bb5c
Lauri Mäkinen <lauri@mits.fi>
2020-03-28 13:36:23 +0200

    Wrap App in to Stack.Navigator

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import MapView from './src/views/MapView';
 
-export default class App extends Component {
-  render() {
-    return (
-      <MapView />
-    );
-  }
-}
+const Stack = createStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Map">
+      <Stack.Screen name="Map" component={MapView} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
---
+++ src/views/LocationView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LocationView = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Location view</Text>
+  </View>
+);
+
+export default LocationView;
